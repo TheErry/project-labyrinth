1e5b34a34465346937bfd6b5113e45db58a00d66
Erika Andersson Porath <eakn@eakn-mac-T2MQ.int.svenskaspel.se>
2022-12-13 23:24:17 +0100

    changed the api links to Google cloud

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,2 @@
-export const API_START = 'https://wk16-backend.herokuapp.com/start'
-export const API_ACTION = 'https://wk16-backend.herokuapp.com/action'
+export const API_START = 'https://labyrinth.technigo.io/start'
+export const API_ACTION = 'https://labyrinth.technigo.io/action'
